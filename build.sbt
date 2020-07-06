42c36198cdd95ae75c53fae011f906788f0a64ec
arainko <aleksander.rainko99@gmail.com>
2020-07-06 17:52:50 +0200

    added Supervisor actor and ImageLoader actor

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "Luxor"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.6"
---
+++ src/main/scala/BrightnessMatrix.scala
@@ -0,0 +1,33 @@
+//import java.awt.image.BufferedImage
+//import java.io.File
+//
+//import javax.imageio.ImageIO
+//
+//type Matrix = Seq[Seq[Int]]
+//type PixelRow = Seq[(Int, Int, Int)]
+//
+////class BrightnessMatrix {
+////  val path = "/home/aleksander/IdeaProjects/Luxor/task/photos/too_dark/h.jpg"
+////
+////  def loadImage: BufferedImage = ImageIO.read(new File(path))
+////
+////  def brightnessRow(image: BufferedImage, row: Int): Matrix = {
+////    val height = image.getHeight
+////    val width = image.getWidth
+////
+////  }
+////
+////}
+//
+//object Test extends App {
+//  val path = "/home/aleksander/IdeaProjects/Luxor/task/photos/too_dark/h.jpg"
+//
+//  def loadImage: BufferedImage = ImageIO.read(new File(path))
+//
+//  def brightnessRow(image: BufferedImage, row: Int): Seq[Int] = {
+//    val height = image.getHeight
+//    val width = image.getWidth
+//
+//  }
+//}
+
