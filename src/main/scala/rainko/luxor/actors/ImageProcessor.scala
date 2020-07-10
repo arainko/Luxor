764b1153f8187ee6266e1a57f7e559db149bef31
Aleksander Rainko <aleksander.rainko99@gmail.com>
2020-07-10 08:43:24 +0200

    added comments to ImageProcessor

--- src/main/scala/rainko/luxor/actors/ImageProcessor.scala
+++ src/main/scala/rainko/luxor/actors/ImageProcessor.scala
@@ -7,8 +7,20 @@ import rainko.luxor.wrappers.Image
 case class BrightnessCalculationRequest(image: Image, pixelRowIndex: Int)
 case class BrightnessResponse(brightness: Brightness)
 
+/**
+ * ImageProcessors calculate average brightness of a pixel row.
+ */
 class ImageProcessor extends Actor {
 
+  /**
+   * Accepts BrightnessCalculationRequests which contains the reference to the image and the row index
+   * of the pixel row to be calculated.
+   *
+   * The actor averages the brightness of the row using Image's brightnessAt(x,y) method
+   * and then sends it to its parent actor.
+   *
+   * Having done that the actor stops itself.
+   */
   override def receive: Receive = {
     case BrightnessCalculationRequest(image, rowIndex) =>
       val pixelRowBrightness: Seq[Brightness] = for (pixelColumn <- 0 until image.width) yield {
